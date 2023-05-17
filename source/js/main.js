ab24820788486eb78e1d6db0b641aa2ab5b5c09a
Dmitry Obryvkov <dmitry.obryvkov213@mail.ru>
2023-05-17 23:56:54 +0300

    Стилизует блок с видео

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
 import {initTabs} from './modules/tabs/init-tabs';
+import {initVideo} from './modules/video/video';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -20,6 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.form = form;
     form.init();
     initTabs();
+    initVideo();
   });
 });
 
