d56538f739dd937627f20b90c4bccf2360127063
Dmitry Obryvkov <dmitry.obryvkov213@mail.ru>
2023-05-20 00:19:09 +0300

    Стилизует слайдер для десктопа и планшета

--- source/js/modules/swipers/swipers.js
+++ source/js/modules/swipers/swipers.js
@@ -13,6 +13,17 @@ const initCoachesSwiper = () => {
         },
 
         breakpoints: {
+          290: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+
+          767: {
+            initialSlide: 2,
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+
           1200: {
             initialSlide: 0,
             slidesPerView: 4,
