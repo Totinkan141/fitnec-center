4916f9608679f08c67da8542d481674925537a5e
Dmitry Obryvkov <dmitry.obryvkov213@mail.ru>
2023-05-24 17:09:14 +0300

    Стилизует всё сделанное под пиксель пёрфект

--- source/js/modules/video/video.js
+++ source/js/modules/video/video.js
@@ -1,15 +1,18 @@
 
 function initVideo() {
-  const parent = document.querySelector('[data-video="parent"]');
-  const video = parent.querySelector('[data-video="element"]');
-  const btnPlay = parent.querySelector('[data-video="button-play"');
+  const parent = document.querySelector('[data-video]');
+  const btnPlay = parent.querySelector('[data-video="button"]');
   const poster = parent.querySelector('[data-video="poster"]');
 
-  if (parent && video && btnPlay && poster) {
+  if (parent && btnPlay && poster) {
     btnPlay.addEventListener('click', () => {
       parent.classList.add('is-active');
-      video.src = 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1';
-      video.style.display = 'block';
+
+      let video = document.createElement('iframe');
+      video.setAttribute('allowfullcreen', '');
+      video.setAttribute('allow', 'autoplay');
+      video.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw/?rel=0&showinfo=0&autoplay=1');
+      parent.appendChild(video);
     });
   }
 }
